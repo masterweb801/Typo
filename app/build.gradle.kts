5b686244799ad8f91bcd387220f8be5b6e1b323c
MD Mobashshirul Karim <masterweb801@gmail.com>
2025-11-26 23:38:38 +0600

    Bug Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,12 +27,12 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+
     kotlinOptions {
-        jvmTarget = "11"
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
     }
     buildFeatures {
         viewBinding = true
